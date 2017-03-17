a1340fe30e485bec9d2e5c9d34df0c41746fe9f0
Alex Wynter <awochna@email.arizona.edu>
2017-03-17 16:45:15 -0700

    Complete word-count

--- word-count/word_count.exs
+++ word-count/word_count.exs
@@ -6,6 +6,16 @@ defmodule Words do
   """
   @spec count(String.t) :: map
   def count(sentence) do
-
+    sentence
+    |> String.downcase
+    |> String.split([" ", "_", ",", "?", "!"])
+    |> Enum.filter(&Regex.run(~r/[\w\d]+/, &1))
+    |> Enum.reduce(%{}, fn(word, counts) ->
+      if (Map.has_key?(counts, word)) do
+        Map.update!(counts, word, &(&1 + 1))
+      else
+        Map.put(counts, word, 1)
+      end
+    end)
   end
 end
