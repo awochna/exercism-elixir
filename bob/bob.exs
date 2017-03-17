c43fbb8224f3421830f59da0f7a0ed62478a6e75
Alex Wynter <awochna@email.arizona.edu>
2017-03-17 16:11:09 -0700

    Complete bob

--- bob/bob.exs
+++ bob/bob.exs
@@ -1,8 +1,22 @@
 defmodule Bob do
   def hey(input) do
     cond do
-        true -> raise "Your implementation goes here"
-
+      silence?(input) -> "Fine. Be that way!"
+      question?(input) -> "Sure."
+      all_caps?(input) -> "Whoa, chill out!"
+      true -> "Whatever."
     end
+  end
+
+  defp silence?(input) do
+    String.trim(input) == ""
+  end
+
+  defp question?(input) do
+    String.last(input) == "?"
+  end
+
+  defp all_caps?(input) do
+    input == String.upcase(input) && String.match?(input, ~r/[^\d, ]+/)
   end
 end
