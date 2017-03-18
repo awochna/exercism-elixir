7d3d05261e5a00e2d734c038816e35d010056188
Alex Wynter <awochna@email.arizona.edu>
2017-03-17 17:19:44 -0700

    Complete acronym

--- acronym/acronym.exs
+++ acronym/acronym.exs
@@ -5,5 +5,18 @@ defmodule Acronym do
   """
   @spec abbreviate(String.t()) :: String.t()
   def abbreviate(string) do
+    string
+    |> String.split([" ", "-", ","])
+    |> Enum.map(fn(word) ->
+      {first, remaining} = String.split_at(word, 1)
+      extras = Regex.run(~r/[A-Z]/, remaining)
+      if (extras) do
+        Enum.join([first] ++ extras)
+      else
+        first
+      end
+    end)
+    |> Enum.join
+    |> String.upcase
   end
 end
