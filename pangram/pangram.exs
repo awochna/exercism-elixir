cd7bf8ba60f887abf140418f905cec1b256b6de4
Alex Wynter <awochna@email.arizona.edu>
2017-03-18 17:58:17 -0700

    Complete pangram

--- pangram/pangram.exs
+++ pangram/pangram.exs
@@ -1,4 +1,6 @@
 defmodule Pangram do
+  @letters ~w(a b c d e f g h i j k l m n o p q r s t u v w x y z)
+
   @doc """
   Determines if a word or sentence is a pangram.
   A pangram is a sentence using every letter of the alphabet at least once.
@@ -11,8 +13,15 @@ defmodule Pangram do
       true
 
   """
-
   @spec pangram?(String.t) :: boolean
   def pangram?(sentence) do
+    sentence
+    |> String.downcase
+    |> String.graphemes
+    |> pangram?(@letters)
+  end
+  def pangram?(charlist, []), do: true
+  def pangram?(charlist, [letter | rest]) do
+    Enum.member?(charlist, letter) && pangram?(charlist, rest)
   end
 end
