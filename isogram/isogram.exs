307360c89b33b4e03b338a5da4fa5a52ce8fe008
Alex Wynter <awochna@email.arizona.edu>
2017-03-19 16:09:34 -0700

    Complete isogram

--- isogram/isogram.exs
+++ isogram/isogram.exs
@@ -3,8 +3,19 @@ defmodule Isogram do
   Determines if a word or sentence is an isogram
   """
   @spec isogram?(String.t) :: boolean
-  def isogram?(sentence) do
-
+  def isogram?(sentence) when is_binary(sentence) do
+    sentence
+    |> String.downcase
+    |> String.graphemes
+    |> Enum.filter(&(Regex.match?(~r/[^-\s]/, &1)))
+    |> isogram?([])
   end
-
+  def isogram?([], _letters), do: true
+  def isogram?([letter | rest], letters) do
+    if (Enum.member?(letters, letter)) do
+      false
+    else
+      isogram?(rest, [letter | letters])
+    end
+  end
 end
