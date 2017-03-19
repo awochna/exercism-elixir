1f6c0b6d9f1d85f98b2654c99bc9d8cfd849dbd8
Alex Wynter <awochna@email.arizona.edu>
2017-03-18 20:42:04 -0700

    Complete nth-prime

--- nth-prime/nth_prime.exs
+++ nth-prime/nth_prime.exs
@@ -4,7 +4,26 @@ defmodule Prime do
   Generates the nth prime.
   """
   @spec nth(non_neg_integer) :: non_neg_integer
+  def nth(0), do: raise ArgumentError
+  def nth(1), do: 2
   def nth(count) do
+    calculate(count)
   end
 
+  defp calculate(count, primes \\ [2], number \\ 3)
+  defp calculate(count, primes, _) when length(primes) == count, do: hd(primes)
+  defp calculate(count, primes, number) do
+    if (is_prime?(number)) do
+      calculate(count, [number | primes], number + 1)
+    else
+      calculate(count, primes, number + 1)
+    end
+  end
+
+  defp is_prime?(candidate) do
+    set = Range.new(2, candidate - 1)
+    !Enum.any?(set, fn(num) ->
+      rem(candidate, num) == 0
+    end)
+  end
 end
