3040bd21d9802be749543a82af8c07c0cc60cdc1
Alex Wynter <awochna@email.arizona.edu>
2017-03-19 14:49:12 -0700

    Complete hamming. Sometimes I wish the biology ones were longer.

--- hamming/hamming.exs
+++ hamming/hamming.exs
@@ -8,6 +8,16 @@ defmodule Hamming do
   {:ok, 4}
   """
   @spec hamming_distance([char], [char]) :: non_neg_integer
-  def hamming_distance(strand1, strand2) do
+  def hamming_distance(strand1, strand2, distance \\ 0)
+  def hamming_distance(s1, s2, _) when length(s1) != length(s2) do
+    {:error, "Lists must be the same length"}
+  end
+  def hamming_distance([], [], distance), do: {:ok, distance}
+  def hamming_distance([n1 | strand1], [n2 | strand2], distance) do
+    if (n1 == n2) do
+      hamming_distance(strand1, strand2, distance)
+    else
+      hamming_distance(strand1, strand2, distance + 1)
+    end
   end
 end
