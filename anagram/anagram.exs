7b02d7a12eab66e54aa48076837d12141c4e5bdc
Alex Wynter <awochna@email.arizona.edu>
2017-03-18 18:52:11 -0700

    Complete anagram

--- anagram/anagram.exs
+++ anagram/anagram.exs
@@ -4,6 +4,20 @@ defmodule Anagram do
   """
   @spec match(String.t, [String.t]) :: [String.t]
   def match(base, candidates) do
+    Enum.filter(candidates, fn(candidate) ->
+      matches?(normalize(base), normalize(candidate))
+    end)
+  end
 
+  defp matches?(base, candidate) when base == candidate, do: false
+  defp matches?(base, candidate) when length(base) == length(candidate) do
+    Enum.sort(base) == Enum.sort(candidate)
+  end
+  defp matches?(_base, _candidate), do: false
+
+  defp normalize(string) do
+    string
+    |> String.downcase
+    |> String.graphemes
   end
 end
