3f5db710e581d7ce64e1c09001b199f8abc9dead
Alex Wynter <awochna@email.arizona.edu>
2017-03-18 20:25:54 -0700

    Complete phone-number

--- phone-number/phone_number.exs
+++ phone-number/phone_number.exs
@@ -18,6 +18,11 @@ defmodule Phone do
   """
   @spec number(String.t) :: String.t
   def number(raw) do
+    raw
+    |> String.graphemes
+    |> clean
+    |> trim
+    |> Enum.join
   end
 
   @doc """
@@ -39,6 +44,12 @@ defmodule Phone do
   """
   @spec area_code(String.t) :: String.t
   def area_code(raw) do
+    raw
+    |> String.graphemes
+    |> clean
+    |> trim
+    |> Enum.take(3)
+    |> Enum.join
   end
 
   @doc """
@@ -60,5 +71,33 @@ defmodule Phone do
   """
   @spec pretty(String.t) :: String.t
   def pretty(raw) do
+    raw
+    |> String.graphemes
+    |> clean
+    |> trim
+    |> insert_pretties
+    |> Enum.join
+  end
+
+  defp clean(graphemes) do
+    if (Enum.any?(graphemes, &(Regex.match?(~r/[^\d-\(\)\.\s]/, &1)))) do
+      zero_out()
+    else
+      Enum.filter(graphemes, &(Regex.match?(~r/\d/, &1)))
+    end
+  end
+
+  defp trim(num) when length(num) == 11 and hd(num) == "1", do: tl(num)
+  defp trim(num) when length(num) == 10, do: num
+  defp trim(_num), do: zero_out()
+
+  defp zero_out(), do: Enum.map(1..10, fn(_) -> 0 end)
+
+  defp insert_pretties(graphemes) do
+    graphemes
+    |> List.insert_at(6, "-")
+    |> List.insert_at(3, " ")
+    |> List.insert_at(3, ")")
+    |> List.insert_at(0, "(")
   end
 end
