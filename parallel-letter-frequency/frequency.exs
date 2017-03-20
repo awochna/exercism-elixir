8ccef0da278b49bc19602faf60360c1fca51e704
Alex Wynter <awochna@email.arizona.edu>
2017-03-20 12:46:10 -0700

    Complete parallel-letter-frequency

--- parallel-letter-frequency/frequency.exs
+++ parallel-letter-frequency/frequency.exs
@@ -8,6 +8,27 @@ defmodule Frequency do
   """
   @spec frequency([String.t], pos_integer) :: map
   def frequency(texts, workers) do
+    texts
+    |> Enum.join
+    |> String.downcase
+    |> String.graphemes
+    |> Enum.chunk(workers, workers, [])
+    |> Enum.map(fn(text) ->
+      Task.async(__MODULE__, :count, [text])
+    end)
+    |> Enum.map(&(Task.await(&1)))
+    |> Enum.reduce(%{}, fn(result_set, acc) ->
+      Map.merge(acc, result_set, fn(_k, v1, v2) -> v1 + v2 end)
+    end)
+  end
 
+  def count(text, acc \\ %{})
+  def count([], acc), do: acc
+  def count([letter | text], acc) do
+    if (Regex.match?(~r/[^\s?\-!\.,1-9]/, letter)) do
+      count(text, Map.update(acc, letter, 1, &(&1 + 1)))
+    else
+      count(text, acc)
+    end
   end
 end
