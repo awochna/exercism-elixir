bc21d4571a5798256ad9f51c8656e4ff85334ec6
Alex Wynter <awochna@email.arizona.edu>
2017-03-20 15:06:47 -0700

    Complete binary.

--- binary/binary.exs
+++ binary/binary.exs
@@ -6,5 +6,24 @@ defmodule Binary do
   """
   @spec to_decimal(String.t) :: non_neg_integer
   def to_decimal(string) do
+    if (Regex.match?(~r/^[01]+$/, string)) do
+      string
+      |> String.graphemes
+      |> Enum.map(&String.to_integer/1)
+      |> reduce_to_decimal
+    else
+      0
+    end
+  end
+
+  defp reduce_to_decimal(digits, acc \\ 0)
+  defp reduce_to_decimal([], acc), do: acc
+  defp reduce_to_decimal([digit | rest], acc) do
+    reduce_to_decimal(rest, acc + (digit * pwr(2, length(rest))))
+  end
+
+  defp pwr(_base, 0), do: 1
+  defp pwr(base, exp) do
+    base * pwr(base, exp - 1)
   end
 end
