1e6934dbbbd9e7fd1bdbe5d2a42f97a93a605613
Alex Wynter <awochna@email.arizona.edu>
2017-03-20 11:50:19 -0700

    Complete grains

--- grains/grains.exs
+++ grains/grains.exs
@@ -3,8 +3,11 @@ defmodule Grains do
   Calculate two to the power of the input minus one.
   """
   @spec square(pos_integer) :: pos_integer
+  def square(number) when number > 64 or number < 1 do
+    {:error, "The requested square must be between 1 and 64 (inclusive)"}
+  end
   def square(number) do
-
+    {:ok, pow(2, number - 1)}
   end
 
   @doc """
@@ -12,6 +15,16 @@ defmodule Grains do
   """
   @spec total :: pos_integer
   def total do
+    {:ok, add(64)}
+  end
 
+  defp pow(_, 0), do: 1
+  defp pow(base, exp) when exp > 0 do
+    base * pow(base, exp - 1)
+  end
+
+  defp add(num) when num == 0, do: 0
+  defp add(num) do
+    pow(2, num - 1) + add(num - 1)
   end
 end
