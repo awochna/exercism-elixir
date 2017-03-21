4780100ea69cc6a252f0ffcc56fce57b8108c34d
Alex Wynter <awochna@awochna.com>
2017-03-21 13:52:16 -0700

    Complete queen-attack

--- queen-attack/queen_attack.exs
+++ queen-attack/queen_attack.exs
@@ -7,8 +7,10 @@ defmodule Queens do
   """
   @spec new() :: Queens.t()
   @spec new({integer, integer}, {integer, integer}) :: Queens.t()
+  def new(white \\ {0, 3}, black \\ {7, 3})
+  def new(white, black) when white == black, do: raise ArgumentError
   def new(white, black) do
-
+    %Queens{black: black, white: white}
   end
 
   @doc """
@@ -17,7 +19,17 @@ defmodule Queens do
   """
   @spec to_string(Queens.t()) :: String.t()
   def to_string(queens) do
-
+    board = Enum.map(1..8, fn(y) ->
+      Enum.map(1..8, fn(x) -> "_" end)
+    end)
+    board = List.update_at(board, elem(queens.white, 0), fn(y) ->
+      List.update_at(y, elem(queens.white, 1), fn(_) -> "W" end)
+    end)
+    board = List.update_at(board, elem(queens.black, 0), fn(y) ->
+      List.update_at(y, elem(queens.black, 1), fn(_) -> "B" end)
+    end)
+    board = Enum.map(board, fn(y) -> Enum.join(y, " ") end)
+    Enum.join(board, "\n")
   end
 
   @doc """
@@ -25,6 +37,19 @@ defmodule Queens do
   """
   @spec can_attack?(Queens.t()) :: boolean
   def can_attack?(queens) do
+    cond do
+      elem(queens.black, 0) == elem(queens.white, 0) ->
+        true
+      elem(queens.black, 1) == elem(queens.white, 1) ->
+        true
+      on_diagonal?(queens.black, queens.white) ->
+        true
+      true ->
+        false
+    end
+  end
 
+  defp on_diagonal?({bx, by}, {wx, wy}) do
+    wx - bx == wy - by || wx + bx == wy + by
   end
 end
