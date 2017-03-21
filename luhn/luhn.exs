a8a489271e32b341d9f8b6942f6503f3a3255a78
Alex Wynter <awochna@awochna.com>
2017-03-21 12:02:46 -0700

    Complete luhn, and interesting exercism and tidbit of information.

--- luhn/luhn.exs
+++ luhn/luhn.exs
@@ -4,6 +4,11 @@ defmodule Luhn do
   """
   @spec checksum(String.t()) :: integer
   def checksum(number) do
+    number
+    |> String.to_integer
+    |> Integer.digits
+    |> check
+    |> Enum.reduce(&(&1 + &2))
   end
 
   @doc """
@@ -11,6 +16,7 @@ defmodule Luhn do
   """
   @spec valid?(String.t()) :: boolean
   def valid?(number) do
+    rem(checksum(number), 10) == 0
   end
 
   @doc """
@@ -19,5 +25,23 @@ defmodule Luhn do
   """
   @spec create(String.t()) :: String.t()
   def create(number) do
+    digit = Enum.find(0..9, fn(digit) ->
+      valid?(number <> Integer.to_string(digit))
+    end)
+    number <> Integer.to_string(digit)
   end
+
+  defp check(digits) do
+    check(Enum.reverse(digits), [])
+  end
+  defp check([], doubled), do: doubled
+  defp check([digit | rest], doubled) when rem(length(doubled), 2) == 1 do
+    check(rest, [double(digit) | doubled])
+  end
+  defp check([digit | rest], doubled) do
+    check(rest, [digit | doubled])
+  end
+
+  defp double(digit) when digit < 5, do: digit * 2
+  defp double(digit), do: digit * 2 - 9
 end
