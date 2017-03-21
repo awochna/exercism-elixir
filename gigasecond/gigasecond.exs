e81bf4083cfd8e28de5e822796bba7a13849315b
Alex Wynter <awochna@awochna.com>
2017-03-21 13:26:27 -0700

    Complete gigasecond

--- gigasecond/gigasecond.exs
+++ gigasecond/gigasecond.exs
@@ -5,6 +5,8 @@ defmodule Gigasecond do
   @spec from({{pos_integer, pos_integer, pos_integer}, {pos_integer, pos_integer, pos_integer}}) :: :calendar.datetime
 
   def from({{year, month, day}, {hours, minutes, seconds}}) do
-
+    {:ok, datetime} = NaiveDateTime.new(year, month, day, hours, minutes, seconds)
+    giga = NaiveDateTime.add(datetime, 1_000_000_000)
+    {{giga.year, giga.month, giga.day}, {giga.hour, giga.minute, giga.second}}
   end
 end
