cbdcc9a976ff59f5277c98a019c6d896651e7232
Alex Wynter <awochna@awochna.com>
2017-03-24 09:07:44 -0700

    Complete pascals-triangle

--- pascals-triangle/pascals_triangle.exs
+++ pascals-triangle/pascals_triangle.exs
@@ -5,5 +5,15 @@ defmodule PascalsTriangle do
   """
   @spec rows(integer) :: [[integer]]
   def rows(num) do
+    Enum.map(1..num, &row_for/1)
+  end
+
+  defp row_for(1), do: [1]
+  defp row_for(2), do: [1, 1]
+  defp row_for(num) do
+    previous = row_for(num - 1)
+    chunks = Enum.chunk(previous, 2, 1)
+    row = Enum.map(chunks, fn([first, second]) -> first + second end)
+    [1] ++ row ++ [1]
   end
 end
