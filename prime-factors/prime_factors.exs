267aaaf257038b2566703edab85cfdc07f6f0b60
Alex Wynter <awochna@awochna.com>
2017-03-25 16:08:25 -0700

    Complete prime-factors

--- prime-factors/prime_factors.exs
+++ prime-factors/prime_factors.exs
@@ -9,6 +9,18 @@ defmodule PrimeFactors do
   """
   @spec factors_for(pos_integer) :: [pos_integer]
   def factors_for(number) do
+    Enum.reverse(factor_out(number))
+  end
 
+  defp factor_out(number, acc \\ [], current \\ 2)
+  defp factor_out(1, _, _), do: []
+  defp factor_out(number, acc, current) when number == current do
+    [current | acc]
+  end
+  defp factor_out(number, acc, current) when rem(number, current) == 0 do
+    factor_out(div(number, current), [current | acc], current)
+  end
+  defp factor_out(number, acc, current) do
+    factor_out(number, acc, current + 1)
   end
 end
