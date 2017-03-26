a59c252830907eb7c76cc71ed663b6600e99e0f7
Alex Wynter <awochna@awochna.com>
2017-03-26 13:03:05 -0700

    Complete bank-account.
    I'm going to go out on a limb and say that this exercism could use some
    more test cases. Perhaps I can submit some.

--- bank-account/account.exs
+++ bank-account/account.exs
@@ -8,11 +8,15 @@ defmodule BankAccount do
   """
   @opaque account :: pid
 
+  use GenServer
+
   @doc """
   Open the bank. Makes the account available.
   """
   @spec open_bank() :: account
   def open_bank() do
+    {:ok, account} = GenServer.start_link(__MODULE__, 0)
+    account
   end
 
   @doc """
@@ -27,6 +31,7 @@ defmodule BankAccount do
   """
   @spec balance(account) :: integer
   def balance(account) do
+    GenServer.call(account, :balance)
   end
 
   @doc """
@@ -34,5 +39,14 @@ defmodule BankAccount do
   """
   @spec update(account, integer) :: any
   def update(account, amount) do
+    GenServer.cast(account, {:update, amount})
+  end
+
+  def handle_call(:balance, _from, state) do
+    {:reply, state, state}
+  end
+
+  def handle_cast({:update, amount}, state) do
+    {:noreply, state + amount}
   end
 end
