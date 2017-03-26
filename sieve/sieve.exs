47c1d9ba1b7c639ffc3e2c3affeab78b82cd261c
Alex Wynter <awochna@awochna.com>
2017-03-25 18:31:54 -0700

    Complete sieve

--- sieve/sieve.exs
+++ sieve/sieve.exs
@@ -5,7 +5,16 @@ defmodule Sieve do
   """
   @spec primes_to(non_neg_integer) :: [non_neg_integer]
   def primes_to(limit) do
-
+    2..limit
+    |> Enum.into([])
+    |> sieve
+    |> Enum.reverse
   end
 
+  defp sieve(nums, acc \\ [])
+  defp sieve([], acc), do: acc
+  defp sieve([num | rest], acc) do
+    filtered = Enum.filter(rest, &(rem(&1, num) != 0))
+    sieve(filtered, [num | acc])
+  end
 end
