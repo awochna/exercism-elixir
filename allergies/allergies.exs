522152a95533bb59aed3eef9f6728403c32e4bf4
Alex Wynter <awochna@awochna.com>
2017-03-29 19:14:00 -0700

    Complete allergies

--- allergies/allergies.exs
+++ allergies/allergies.exs
@@ -1,10 +1,15 @@
 defmodule Allergies do
+  @allergies [:eggs, :peanuts, :shellfish, :strawberries, :tomatoes, :chocolate, :pollen, :cats]
+
   @doc """
   List the allergies for which the corresponding flag bit is true.
   """
   @spec list(non_neg_integer) :: [String.t]
   def list(flags) do
-
+    flags
+    |> Integer.digits(2)
+    |> Enum.reverse
+    |> construct_list
   end
 
   @doc """
@@ -12,6 +17,15 @@ defmodule Allergies do
   """
   @spec allergic_to?(non_neg_integer, String.t) :: boolean
   def allergic_to?(flags, item) do
+    flags
+    |> list
+    |> Enum.member?(item)
+  end
 
+  defp construct_list(bits) do
+    [@allergies, bits]
+    |> Enum.zip
+    |> Enum.reject(fn({_k, v}) -> v == 0 end)
+    |> Enum.map(fn({k, _v}) -> Atom.to_string(k) end)
   end
 end
