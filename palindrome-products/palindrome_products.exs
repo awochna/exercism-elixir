b3a7701a98c1ee54dbf1880b0f194dd0f22adb4a
Alex Wynter <awochna@awochna.com>
2017-03-29 19:40:14 -0700

    Complete palindrome-products.

--- palindrome-products/palindrome_products.exs
+++ palindrome-products/palindrome_products.exs
@@ -5,5 +5,36 @@ defmodule Palindromes do
   """
   @spec generate(non_neg_integer, non_neg_integer) :: map
   def generate(max_factor, min_factor \\ 1) do
+    range = Range.new(min_factor, max_factor)
+    Range.new(min_factor * min_factor, max_factor * max_factor)
+    |> Enum.filter(&palindrome?/1)
+    |> Enum.map(fn(palindrome) ->
+      factors = Enum.filter(range, fn(factor) ->
+        rem(palindrome, factor) == 0 && div(palindrome, factor) in range
+      end)
+      if Enum.empty?(factors) do
+        {}
+      else
+        {palindrome, other_factors(factors, palindrome)}
+      end
+    end)
+    |> Enum.reject(fn(tuple) -> tuple_size(tuple) == 0 end)
+    |> Enum.into(%{})
+  end
+
+  defp other_factors(factors, palindrome) do
+    factors
+    |> Enum.map(fn(factor) ->
+      Enum.sort([factor, div(palindrome, factor)])
+    end)
+    |> Enum.uniq
+  end
+
+  defp palindrome?(number) do
+    reversed = number
+    |> Integer.digits
+    |> Enum.reverse
+    |> Integer.undigits
+    reversed == number
   end
 end
--- palindrome-products/palindrome_products_test.exs
+++ palindrome-products/palindrome_products_test.exs
@@ -15,28 +15,28 @@ defmodule PalindromeProductsTest do
     assert Enum.sort(palindromes[9]) == [[1, 9], [3, 3]]
   end
 
-  @tag :pending
+  # @tag :pending
   test "largest palindrome from double digit factors" do
     palindromes = Palindromes.generate(99, 10)
     assert (palindromes |> Map.keys |> Enum.sort |> List.last) == 9009
     assert palindromes[9009] == [[91, 99]]
   end
 
-  @tag :pending
+  # @tag :pending
   test "smallest palindrome from double digit factors" do
     palindromes = Palindromes.generate(99, 10)
     assert (palindromes |> Map.keys |> Enum.sort |> hd) == 121
     assert palindromes[121] == [[11, 11]]
   end
 
-  @tag :pending
+  # @tag :pending
   test "largest palindrome from triple digit factors" do
     palindromes = Palindromes.generate(999, 100)
     assert (palindromes |> Map.keys |> Enum.sort |> List.last) == 906609
     assert palindromes[906609] == [[913, 993]]
   end
 
-  @tag :pending
+  # @tag :pending
   test "smallest palindromes from triple digit factors" do
     palindromes = Palindromes.generate(999, 100)
     assert (palindromes |> Map.keys |> Enum.sort |> hd) == 10201
